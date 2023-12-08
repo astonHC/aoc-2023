150964ba5e2d0840a7942c54eea1947dbaf63621
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-12-08 23:50:44 +0000

    initial commit

---
+++ DAY_1/main.c
@@ -0,0 +1,65 @@
+/* COPYRIGHT (C) HARRY CLARK 2023 */
+
+/* ADVENT OF CODE 2023 */
+
+/* THE FOLLOWING FILE PERTAINS TO THE SYNOPSIS OF THE TASK FOR DAY 1 */
+
+/* SYSTEM INCLUDES */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef struct INPUTS
+{
+    const char* INPUT;
+    const char* DIGIT_MAP[0];
+    int FIRST;
+    int LAST;
+
+    int RESULT = 0;
+    int MAP_LENGTH;
+
+} INPUTS;
+
+/* CALIBRATE THE CORRESPONDING VALUES TO GET THE WEATHER MACHINE */
+/* WORKING TO A SUFFICIENT STATE */
+
+static int CALIBRATE_DIGITS()
+{
+    struct INPUTS* INPUT_VALUES;
+
+    for (int i = 0; i < strlen(INPUT_VALUES->INPUT); i++)
+    {
+        if(isdigit(INPUT_VALUES->INPUT[i]))
+        {
+            INPUT_VALUES->FIRST = INPUT_VALUES->INPUT[i] - '0';
+            INPUT_VALUES->LAST = INPUT_VALUES->INPUT[i] - '0';
+        }
+    }
+
+    return INPUT_VALUES->FIRST * 10 + INPUT_VALUES->LAST;
+}
+
+/* MAP OUT THE CORRESPONDING VALUES TO CALIBRATE THE ABOVE */
+
+static int MAP_VALUES()
+{
+    struct INPUTS* DIGIT_INPUT;
+
+    const char* MAP[] =
+    {
+        "ONE",
+        "TWO",
+        "THREE",
+        "FOUR",
+        "FIVE",
+        "SIX",
+        "SEVEN",
+        "EIGHT",
+        "NINE",
+    };
+
+
+};
